c0b2ed3fa1e1bb5a0b5864cb9330694b3e17a2fe
zbolmb <zhijianli@gatech.edu>
2016-04-02 11:29:46 -0400

    added object

---
+++ Object.py
@@ -0,0 +1,10 @@
+class Object(object):
+
+	# Class Object Attribute
+	
+	def __init__(self, mass, velocity, density, restitution):
+		self.mass = mass
+		self.velocity = velocity
+		self.density = density
+		self.restitution = restitution
+
