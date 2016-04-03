e0e7bf0079d2ea07937daf837c04d25cfa2a2bc2
zbolmb <zhijianli@gatech.edu>
2016-04-03 17:20:14 -0400

    added controller

---
+++ controller/GuiController.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+
+public class GuiController extends Application {
+
+	public void start(Stage primary) throws exception { 
+		GuiView Gui = new GuiView(new GridPane(500, 500));
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
